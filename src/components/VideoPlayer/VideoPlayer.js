7cd27f17981f3a1566b5826effa4e74172feeed2
Michael Cacciano <michael.cacciano@gmail.com>
2020-09-05 21:09:06 -0400

    add controls to player

--- src/components/VideoPlayer/VideoPlayer.js
+++ src/components/VideoPlayer/VideoPlayer.js
@@ -3,10 +3,15 @@ import PropTypes from 'prop-types';
 import ReactPlayer from 'react-player/youtube';
 
 const VideoPlayer = ({ url }) => {
+  const ytPlayerConfig = {
+    url,
+    controls: true
+  };
+
   return (
     <div className='flex justify-center w-full bg-black'>
       <div className='flex justify-center w-full max-w-screen-lg'>
-        <ReactPlayer url={url} />
+        <ReactPlayer {...ytPlayerConfig} />
       </div>
     </div>
   );
