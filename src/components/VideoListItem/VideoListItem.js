22432cb606ba7333c5ebb6436dc28198b9ef1eef
Michael Cacciano <michael.cacciano@gmail.com>
2020-09-05 21:36:48 -0400

    add hover animation and overlay to video items

--- src/components/VideoListItem/VideoListItem.js
+++ src/components/VideoListItem/VideoListItem.js
@@ -1,12 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 
 const VideoListItem = ({ video, onClick }) => {
+  const [hover, setHover] = useState(false);
+
   return (
-    <li className='flex flex-col border border-black rounded shadow' onClick={onClick}>
-      <div className='flex-1'>
+    <li
+      className='flex flex-col border border-black rounded shadow cursor-pointer transform duration-1000 ease-out hover:scale-105 hover:shadow-lg'
+      onClick={onClick}
+      onMouseEnter={() => setHover(true)}
+      onMouseLeave={() => setHover(false)}
+    >
+      <div className='overflow-hidden relative'>
+        <div
+          className={`absolute inset-0 bg-black opacity-25 w-full h-full ${hover ? 'hidden' : ''}`}
+        ></div>
         <img
-          className='w-full h-full object-cover'
+          className={`w-full h-full object-cover`}
           src={video.snippet.thumbnails.medium.url}
           alt={video.snippet.title}
         />
@@ -17,9 +27,8 @@ const VideoListItem = ({ video, onClick }) => {
           <p className='mt-4 text-md font-light'>
             {video.snippet.description
               .split(' ')
-              .map((des, i) => (i < 10 ? des : null))
+              .map((des, i) => (i < 20 ? des : null))
               .join(' ')}
-            ...
           </p>
         ) : null}
       </div>
